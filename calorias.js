8be75890b407601872bcef4733d6b62f27b230e0
Linguineproblemas <lsmiguel@aluno.crb.g12.br>
2024-11-01 12:42:01 -0300

    Update and rename calorias.py to calorias.js

---
+++ calorias.js
@@ -0,0 +1,8 @@
+function CalcularCalorais():
+     const checkboxes = document.querySelectorAll('.food:checked');
+            let totalCalorias = 0;
+            checkboxes.forEach(checkbox => {
+                totalCalorias += parseInt(checkbox.value);
+            });
+            document.getElementById('resultado').innerText = `Total de Calorias: ${totalCalorias} Kcal`;
+        }
